29f2c96225a6e9415ef6df820af5e89a6ff4806f
Taioh Kubota <kubotataioh@gmail.com>
2023-04-07 18:12:07 -0400

    created simple tic-tac-toe game

---
+++ app.js
@@ -0,0 +1,63 @@
+const gameBoard = document.querySelector('#gameboard')
+const infoDisplay = document.querySelector('#info')
+const startCells = [
+    "", "", "","", "", "","", "", ""
+]
+
+let go = "circle"
+
+infoDisplay.textContent = "Circle goes first"
+
+function createBoard(){
+    for(let i = 0 ; i<startCells.length; i++){
+        const cellElement = document.createElement('div')
+        cellElement.classList.add('square')
+        cellElement.id = i
+        cellElement.addEventListener('click', addGo)
+        gameBoard.append(cellElement)
+    }
+}
+
+function addGo(e){
+    const goDisplay = document.createElement("div")
+    goDisplay.classList.add(go)
+    e.target.append(goDisplay)
+    go = go === "circle" ? "cross" : "circle"
+    infoDisplay.textContent = `It is now ${go}'s turn`
+    e.target.removeEventListener("click", addGo)
+    checkScore()
+}
+
+function checkScore(){
+    const allSquares = document.querySelectorAll(".square")
+    const winningCombos = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+
+    winningCombos.forEach(array =>{
+       let circleWins= array.every(cell =>
+            allSquares[cell].firstChild?.classList.contains('circle'))
+
+        if(circleWins){
+            infoDisplay.textContent = "Circle Wins"
+            allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+            return
+        }
+    })
+
+    winningCombos.forEach(array =>{
+        let crossWins= array.every(cell =>
+             allSquares[cell].firstChild?.classList.contains('cross'))
+
+         if(crossWins){
+             infoDisplay.textContent = "Cross Wins"
+             allSquares.forEach(square => square.replaceWith(square.cloneNode(true)))
+             return
+         }
+     })
+
+}
+
+createBoard()
